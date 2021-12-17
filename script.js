e4727e7077a8f694e7c273aa061148019a730d8f
Grant Royseth <groyseth@gmail.com>
2021-12-16 18:55:14 -0600

    working code

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 var generateBtn = document.querySelector("#generate");
 
-// var numeric = ["1","2","3","4","5","6","6","7","8","9","0"]
-// var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-// var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-// var symbol = ["!","@","#","$","%","^","&","*","(",")","_","-","="]
 
-
-
-// Write password to the #password input
 
 
 function generatePassword() {
@@ -15,7 +8,7 @@ function generatePassword() {
   var passwordLength;
   var collect = []
   
-  
+  // possible variables 
   var numeric = ["1","2","3","4","5","6","7","8","9","0"]
   var capText = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
   var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -24,9 +17,9 @@ function generatePassword() {
 
 
 
-  var numOfChar = prompt("enter the length of password");
+  var numOfChar = prompt("Please enter the length of password, must be inbetween 8 and 128!");
   if (!(numOfChar >= 8 && numOfChar <= 128)){
-    alert("no try again")
+    alert("Please enter within the paramiters")
     return;
   }else{
     passwordLength = numOfChar;
@@ -34,39 +27,35 @@ function generatePassword() {
     
       
   
-  var upCase = confirm("would you like Uppercase?");
+  var upCase = confirm("Would you like Uppercase?");
   if (upCase){
     userPic = userPic.concat(capText);
   }
   
-  var lowCase = confirm("would you like lowercase?");
+  var lowCase = confirm("Would you like lowercase?");
   if (lowCase){
     userPic = userPic.concat(lowerChar)
   }
   
-  var numbersVar = confirm("would you like numeric?");
+  var numbersVar = confirm("Would you like numeric?");
   if (numbersVar){
     userPic = userPic.concat(numeric)
   }
   
   
-  var specialChar = confirm("would you like characters?")
+  var specialChar = confirm("Would you like characters?")
   if (specialChar){
     userPic = userPic.concat(symbol)
   }
   
   
-  // userPic.push(collect);
+  // loop the user array and empty and password lenght 
   for (var i = 0; i < passwordLength; i++) {
     var charAmount = userPic[(Math.floor((Math.random() * userPic.length)))]
     collect.push(charAmount);
   }
    
-  // userPic = (Math.floor((Math.random() * passwordLength.length)));
-  // }
-  
-  // pulling a random character from userPics for each value of passwordLength
-  // passwordLength.push(userPic);
+ 
  var passwordText = collect.join("");
   collect=[]
 
@@ -76,27 +65,6 @@ function generatePassword() {
  
   return passwordText;
 }  
-
-   
-  
-
-  //  var randNum = Math.floor(Math.random() *numBs.length);
-  // console.log(numOfChar);
-  // add "prompt function" for userInput for num of char in password
-  // 8>=num<128
-  // var userInput = prompt("lowercase?");
-  // if (userInput )
-  
-  //userInput confirmation for numeric, lowercase, uppercase, special (separately)
-  //save those confirmations in separate variables
-  // store all the values in form of arrays
-  // var numeric = [0,1,2,3...]
-  //based on user confirmation pick the correct arrays and concatenate them and store that concat array in new    variable
-  //loop num times
-  //generate random num for concat array and grab the item
-  //push that item in new password array
-  // return new password
-
 
 function writePassword() {
   
@@ -112,3 +80,22 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+  //  var randNum = Math.floor(Math.random() *numBs.length);
+  // add "prompt function" for userInput for num of char in password
+  // 8>=num<128
+  // var userInput = prompt("lowercase?");
+  // if (userInput )
+  //userInput confirmation for numeric, lowercase, uppercase, special (separately)
+  //save those confirmations in separate variables
+  // store all the values in form of arrays
+  // var numeric = [0,1,2,3...]
+  //based on user confirmation pick the correct arrays and concatenate them and store that concat array in new    variable
+  //loop num times
+  //generate random num for concat array and grab the item
+  //push that item in new password array
+  // return new password
